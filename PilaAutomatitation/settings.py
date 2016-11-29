90b768cdf1f4b6e59a2103dfe5c2649c2707c0fd
Henry Mora <hank8617@gmail.com>
2016-11-29 01:58:32 -0500

    Cambio de base de datos

--- PilaAutomatitation/settings.py
+++ PilaAutomatitation/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -80,10 +82,12 @@ WSGI_APPLICATION = 'PilaAutomatitation.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+    'default':
+        dj_database_url.config(default= os.environ.get('DATABASE_URL'))
 }
 
 
